0d73f5a73226f34739e725f51f494a6161aa66a5
羽白不白 <949362495@qq.com>
2026-09-27 02:18:10 +0800

    版本号修改

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 var (
 	// Version is the current File Browser version.
-	Version = "(untracked)"
+	Version = "3.0.0-HY"
 	// CommitSHA is the commit sha.
 	CommitSHA = "(unknown)"
 )
